13fff5bd07a22c7093ab65e3998c4a2c87176edc
KEYSIGHT\alexwang <alexander.wang@keysight.com>
2021-01-03 18:19:19 -0500

    upload all src and e2e

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TitlebarComponent } from './titlebar/titlebar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    TitlebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
